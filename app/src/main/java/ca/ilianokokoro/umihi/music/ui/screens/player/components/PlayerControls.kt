269f6bc0fbd2580bf36879dc30d63890f20a05a9
Developer <developer@example.com>
2026-05-21 01:31:01 +0530

    Fix unresolved reference compile errors in PlayerControls by importing Box and basicMarquee

--- app/src/main/java/ca/ilianokokoro/umihi/music/ui/screens/player/components/PlayerControls.kt
+++ app/src/main/java/ca/ilianokokoro/umihi/music/ui/screens/player/components/PlayerControls.kt
@@ -5,6 +5,8 @@ import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
